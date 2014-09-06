06bf22a242f7dd413044fa6fe10295a814393b8d
cocagolau <cocagolau@gmail.com>
2014-09-06 21:22:46 +0900

    dao transaction

--- jwp_qna/src/test/java/me/dec7/dao/users/UserDaoTest.java
+++ jwp_qna/src/test/java/me/dec7/dao/users/UserDaoTest.java
@@ -11,13 +11,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/applicationContext.xml")
-public class MyBatisUserDaoTest {
+// transaction 설정
+@TransactionConfiguration(transactionManager="transactionManager", defaultRollback=true)
+/*
+ *  해당 class를 transaction을 처리하기 위한 annotation
+ *  해당 class의 method를 모두 실행한 다음 / 해당 데이터들은 자동적으로 rollback이 됨
+ */
+// @Transactional
+public class UserDaoTest {
 
-	private static final Logger log = LoggerFactory.getLogger(MyBatisUserDaoTest.class);
+	private static final Logger log = LoggerFactory.getLogger(UserDaoTest.class);
 	
 	@Autowired
 	private UserDao userDao;
@@ -30,8 +39,12 @@ public class MyBatisUserDaoTest {
 	}
 	
 	@Test
+	/*
+	 * transactional level를 class 단위가 아닌 method 단위에서도 설정 가능
+	 */
+	@Transactional
 	public void create() throws Exception {
-		User user = new User("dec8", "pw", "dec", "dec@gmail.com");
+		User user = new User("dec888", "pw", "dec", "dec@gmail.com");
 		userDao.create(user);
 		User actual = userDao.findById(user.getUserId());
 		
